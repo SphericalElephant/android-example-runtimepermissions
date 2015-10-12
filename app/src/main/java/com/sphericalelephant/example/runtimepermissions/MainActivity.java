f28ab1fda888b215bb79ce04847389f6bdd0f29c
Patrick Sturm <siyb@geekosphere.org>
2015-10-12 21:45:23 +0200

    example code done (for now)

--- app/src/main/java/com/sphericalelephant/example/runtimepermissions/MainActivity.java
+++ app/src/main/java/com/sphericalelephant/example/runtimepermissions/MainActivity.java
@@ -1,24 +1,74 @@
 package com.sphericalelephant.example.runtimepermissions;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 /**
- * Created by siyb on 12/10/15.
+ * We are implementing ActivityCompat.OnRequestPermissionsResultCallback in order to be able to receive
+ * permission callbacks
  */
-public class MainActivity extends AppCompatActivity {
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_mainactivity);
-		findViewById(R.id.activity_mainactivity_b_requestpermissions).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
 
-			}
-		});
-	}
+    private static final String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+    private static final int REQUEST_CODE = 1;
 
+    private TextView infoText;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainactivity);
+        findViewById(R.id.activity_mainactivity_b_requestpermissions).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (coarseLocationDenied() || fineLocationDenied()) {
+                    ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS, REQUEST_CODE);
+                }
+            }
+        });
+
+        infoText = (TextView) findViewById(R.id.activity_mainactivity_tv_info);
+    }
+
+    private boolean coarseLocationDenied() {
+        return ContextCompat.checkSelfPermission(
+                getApplicationContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_DENIED;
+    }
+
+    private boolean fineLocationDenied() {
+        return ContextCompat.checkSelfPermission(
+                getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        System.err.println("onActivityResult gets called: " + requestCode + " " + resultCode + " " + data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_CODE) { // verify that we are dealing with the correct request
+            StringBuilder text = new StringBuilder();
+            for (int i = 0; i < permissions.length; i++) {
+                text.append(permissions[i]);
+                text.append(' ');
+                text.append(
+                        grantResults[i] == PackageManager.PERMISSION_DENIED ?
+                                getString(R.string.activity_mainactivity_denied) :
+                                getString(R.string.activity_mainactivity_granted));
+                text.append('\n');
+            }
+            infoText.setText(text);
+        }
+    }
 }
